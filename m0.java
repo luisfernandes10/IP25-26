e38495673bf73ed279f2c082ac3eeb259b0e881d
Luís Fernandes <lcsfs@iscte-iul.pt>
2025-09-15 21:41:07 +0100

    Código do módulo m0

--- m0.java
+++ m0.java
@@ -1,2 +1,80 @@
 class m0 {
+
+    static void vagas() {
+        int lei = 90;
+        int lige = 60;
+        int leti = 60;
+        int total = lei + lige + leti;
+    }
+
+    static void finalGrade() {
+        int testeIntercalar = 67;
+        int projeto = 85;
+        int testeFinal = 78;
+        double notaFinal = (testeIntercalar * 0.25) + (projeto * 0.15) + (testeFinal * 0.6);
+    }
+
+    static void intArea() {
+        int width = 10;
+        int height = 15;
+        int area = width * height;
+    }
+
+    static void varCopy() {
+        int a = 7;
+        int b = a;
+        a = a + 5;
+        b = b + 3;
+    }
+
+    static void doubleKeyword() {
+        int n = 10;
+        int doubleN = n * 2;
+    }
+
+    static void caseSensitive() {
+        int n = 10;
+        int doubleN = n * 2;
+    }
+
+    static void piConstant() {
+        final double PI = 3.142;
+        int r = 5;
+        double area = PI * r * r;
+    }
+
+    static void integerDivision() {
+        int sevenDivTwo = 7 / 2;
+        int sevenModTwo = 7 % 2;
+    }
+
+    static void counterVar() {
+        int c = 0;
+        c = 1;
+        c = 2;
+        c = 3;
+    }
+
+    static void power2Var() {
+        int p = 1;
+        p = p * 2;
+        p = p * 2;
+        p = p * 2;
+        p = p * 2;
+    }
+
+    static void swapVar() {
+        int a = 5;
+        int b = 7;
+        int temp = 0;
+        temp = a;
+        a = b;
+        b = temp;
+    }
+
+    static void roundGrade() {
+        int notaZeroCem = 79;
+        double notaZeroVinte = (double) (notaZeroCem * 20) / 100;
+        int notaZeroVinteArr = (int) Math.round(notaZeroVinte);
+    }
 }
